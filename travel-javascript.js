f566349b3c502951f5a91c01df9ab89e1ecd1300
Taiwo Akinnusoye <34939115+TaiwoAkinnusoye@users.noreply.github.com>
2018-06-25 17:21:47 +0100

    Update travel-javascript.js

--- travel-javascript.js
+++ travel-javascript.js
@@ -1,6 +1,26 @@
-$('.collapsed-txt').readmore({
-  speed: 200,
-  collapsedHeight: 240,
-  moreLink: '<a href="#">More</a>',
-  lessLink: '<a href="#">Less</a>',
-  });
+    $(document).ready(function(){
+          $('.fading').slick({
+  dots: false,
+  infinite: true,
+  speed: 4000,
+  fade: true,
+  cssEase: 'linear',
+autoplay: true
+});
+});
+
+
+  $(function(){
+      $('.sliding-pics').slick({
+infinite: true,
+slidesToShow: 4,
+slidesToScroll: 3,
+autoplay: true,
+  autoplaySpeed: 1000
+      });
+    });
+
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+});
